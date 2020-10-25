f76d37f78b84e70712ca8ac82fe3eaa93aee8e0f
Bojan <page0itv@gmail.com>
2020-10-25 18:53:55 +0100

    Add play black samurai method

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,16 +7,32 @@ public class GameManager : MonoBehaviour
 {
     public bool IsGameActive { get; set; }
 
+    [SerializeField] GameObject blackSamurai;
+
     CharacterController characterController;
+    SoundManager soundManager;
 
     // Awake is called when the script instance is being loaded
     private void Awake()
     {
         characterController = GameObject.Find("Player").GetComponent<CharacterController>();
+        soundManager = GameObject.Find("SoundManager").GetComponent<SoundManager>();
     }
 
     public void StartGame()
     {
         IsGameActive = true;
+    }
+
+    // Update is called every frame, if the MonoBehaviour is enabled
+    private void Update()
+    {
+        Debug.Log(Enemy.deathCount);
+        if(Enemy.deathCount >= 3)
+        {
+            blackSamurai.SetActive(true);
+            soundManager.PlayBlackSamurai();
+            Enemy.deathCount = 0;
+        }
     }
 }
