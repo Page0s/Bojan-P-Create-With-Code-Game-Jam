eaa0f58a3083c2d2803531d794e49c2806b5e055
Bojan <page0itv@gmail.com>
2020-10-25 17:15:29 +0100

    Add Rotate.cs

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField] float rotateSpeed = 2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+    }
+}
