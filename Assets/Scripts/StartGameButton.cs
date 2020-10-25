7d7a775b7f9b966da67ccb759f55d0cfa5980171
Bojan <page0itv@gmail.com>
2020-10-25 18:06:18 +0100

    Add StartGameButton.cs

---
+++ Assets/Scripts/StartGameButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartGameButton : MonoBehaviour
+{
+    Button button;
+    GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        button = GetComponent<Button>();
+        button.onClick.AddListener(StartGame);
+    }
+
+    private void StartGame()
+    {
+        Debug.Log("Game Strted!");
+        gameManager.StartGame();
+
+        Button[] buttons = FindObjectsOfType<Button>();
+
+        foreach (Button button in buttons)
+            button.gameObject.SetActive(false);
+    }
+}
